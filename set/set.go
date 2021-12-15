ef00e34b76b8624741854343fc1e32a5a9d9587c
Zachary Yedidia <zyedidia@gmail.com>
2021-12-15 15:10:49 -0500

    Add hashset

---
+++ set/set.go
@@ -0,0 +1,27 @@
+package set
+
+import g "github.com/zyedidia/generic"
+import "github.com/zyedidia/generic/hashmap"
+
+type Set[K g.Hashable[K]] struct {
+	m *hashmap.Map[K, struct{}]
+}
+
+func NewSet[K g.Hashable[K]](capacity uint64) *Set[K] {
+	return &Set[K]{
+		m: hashmap.NewMap[K, struct{}](capacity),
+	}
+}
+
+func (s *Set[K]) Put(val K) {
+	s.m.Set(val, struct{}{})
+}
+
+func (s *Set[K]) Has(val K) bool {
+	_, ok := s.m.Get(val)
+	return ok
+}
+
+func (s *Set[K]) Remove(val K) {
+	s.m.Delete(val)
+}

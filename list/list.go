e103518f3788ccc0e7f51eed287cccf1d0b94975
Zachary Yedidia <zyedidia@gmail.com>
2021-12-15 21:09:03 -0500

    list iterator

--- list/list.go
+++ list/list.go
@@ -1,5 +1,7 @@
 package list
 
+import "github.com/zyedidia/generic/iter"
+
 type List[V any] struct {
 	Head, Tail *Node[V]
 }
@@ -30,5 +32,27 @@ func (l *List[V]) Remove(n *Node[V]) {
 		n.Prev.Next = n.Next
 	} else {
 		l.Head = n.Next
+	}
+}
+
+func (n *Node[V]) Iter() iter.Iter[V] {
+	node := n
+	return func() (v V, ok bool) {
+		if node == nil {
+			return v, false
+		}
+		node = node.Next
+		return node.Value, true
+	}
+}
+
+func (n *Node[V]) ReverseIter() iter.Iter[V] {
+	node := n
+	return func() (v V, ok bool) {
+		if node == nil {
+			return v, false
+		}
+		node = node.Prev
+		return node.Value, true
 	}
 }

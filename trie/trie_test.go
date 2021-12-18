9a97d2d400784623d0a0ac9a6825cb8721ec2093
Zachary Yedidia <zyedidia@gmail.com>
2021-12-17 22:29:01 -0500

    Cross check test for trie

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -2,9 +2,62 @@ package trie_test
 
 import (
 	"fmt"
+	"math/rand"
+	"testing"
 
 	"github.com/zyedidia/generic/trie"
 )
+
+var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randstring(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func checkeq(tr *trie.Trie[int], m map[string]int, t *testing.T) {
+	for k, v := range m {
+		tv, ok := tr.Get(k)
+		if !ok {
+			t.Fatalf("%v should exist", k)
+		}
+		if tv != v {
+			t.Fatalf("%v != %v, key: %v", tv, v, k)
+		}
+	}
+}
+
+func TestCrossCheck(t *testing.T) {
+	stdm := make(map[string]int)
+	tree := trie.New[int]()
+
+	const nops = 1000
+
+	for i := 0; i < nops; i++ {
+		key := randstring(rand.Intn(20) + 1)
+		val := rand.Int()
+		op := rand.Intn(2)
+
+		switch op {
+		case 0:
+			stdm[key] = val
+			tree.Put(key, val)
+		case 1:
+			var del string
+			for k := range stdm {
+				del = k
+				break
+			}
+			delete(stdm, del)
+			tree.Remove(del)
+		}
+
+		checkeq(tree, stdm, t)
+	}
+}
 
 func Example() {
 	tr := trie.New[int]()

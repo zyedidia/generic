e63b282f39ba91dc1d78066515a08fef4d3418b6
Jan Dubský <honzadubsky3@seznam.cz>
2022-04-24 22:31:52 +0200

    [interval] Expose interval information from tree API (#14)
    * Fix removal with copy-on-write
    
    * [internal] Allow arbitrary ordered type to represent an interval
    
    * [interval] Expose interval information from tree API
    
    Co-authored-by: Zachary Yedidia <zyedidia@gmail.com>

--- interval/itree.go
+++ interval/itree.go
@@ -17,176 +17,257 @@
 package interval
 
 import (
-	g "github.com/zyedidia/generic"
+	"fmt"
+
+	"github.com/zyedidia/generic"
+	"golang.org/x/exp/constraints"
 )
 
-type KV[V any] struct {
-	Low, High int
+type KV[I constraints.Ordered, V any] struct {
+	Low, High I
 	Val       V
 }
 
-// intrvl represents an interval over [low, high).
-type intrvl struct {
-	Low, High int
+func newKV[I constraints.Ordered, V any](n *node[I, V]) KV[I, V] {
+	return KV[I, V]{
+		Low:  n.key.low,
+		High: n.key.high,
+		Val:  n.value,
+	}
 }
 
-func overlaps(i1 intrvl, low, high int) bool {
-	return i1.Low < high && i1.High > low
+// intrvl represents an interval over [low, high).
+type intrvl[I constraints.Ordered] struct {
+	low, high I
+}
+
+func newIntrvl[I constraints.Ordered](low, high I) intrvl[I] {
+	if low > high {
+		panic(fmt.Sprintf("low cannot be greater than high: %v > %v", low, high))
+	}
+	return intrvl[I]{low, high}
+}
+
+func overlaps[I constraints.Ordered](i1 intrvl[I], i2 intrvl[I]) bool {
+	return i1.low < i2.high && i1.high > i2.low
 }
 
 // Tree implements an interval tree. All intervals must have unique starting
-// positions.
-type Tree[V any] struct {
-	root *node[V]
+// positions. Every low bound if an interval is inclusive, while high is
+// exclusive.
+type Tree[I constraints.Ordered, V any] struct {
+	root *node[I, V]
 }
 
 // New returns an empty interval tree.
-func New[V any]() *Tree[V] {
-	return &Tree[V]{}
+func New[I constraints.Ordered, V any]() *Tree[I, V] {
+	return &Tree[I, V]{}
 }
 
-// Put associates the interval 'key' with 'value'.
-func (t *Tree[V]) Put(low, high int, value V) {
-	t.root = t.root.add(intrvl{low, high}, value)
+// Add associates the interval [low, high) with value.
+//
+// If an interval starting at low already exists in t, this method doesn't
+// perform any change of the tree, but returns the conflicting interval.
+func (t *Tree[I, V]) Add(low, high I, value V) (KV[I, V], bool) {
+	newRoot, kv, ok := t.root.insert(newIntrvl(low, high), value, false)
+	t.root = newRoot
+	return kv, ok
 }
 
-// Overlaps returns all values that overlap with the given range.
-func (t *Tree[V]) Overlaps(low, high int) []V {
-	var result []V
-	return t.root.overlaps(intrvl{low, high}, result)
+// Put associates the interval [low, high) with value.
+//
+// If an interval starting at low already exists, this method will replace it.
+// In such a case the conflicting (replaced) interval is returned.
+func (t *Tree[I, V]) Put(low, high I, value V) (KV[I, V], bool) {
+	newRoot, kv, ok := t.root.insert(newIntrvl(low, high), value, true)
+	t.root = newRoot
+	return kv, ok
 }
 
-// Remove deletes the interval starting at 'pos'.
-func (t *Tree[V]) Remove(pos int) {
-	t.root = t.root.remove(pos)
+// Overlaps returns all values that overlap with the given range. List returned
+// is sorted by low positions of intervals.
+func (t *Tree[I, V]) Overlaps(low, high I) []KV[I, V] {
+	return t.root.overlaps(newIntrvl(low, high), nil)
 }
 
-// Get returns the value associated with the interval starting at 'pos', or
-// 'false' if no such value exists.
-func (t *Tree[V]) Get(pos int) (V, bool) {
-	n := t.root.search(pos)
+// Remove deletes the interval starting at low. The removed interval is
+// returned. If no such interval existed in a tree, the returned value is false.
+func (t *Tree[I, V]) Remove(low I) (KV[I, V], bool) {
+	newRoot, kv, ok := t.root.remove(low)
+	t.root = newRoot
+	return kv, ok
+}
+
+// Get returns the interval and value associated with the interval starting at
+// low, or false if no such value exists.
+func (t *Tree[I, V]) Get(low I) (KV[I, V], bool) {
+	n := t.root.search(low)
 	if n == nil {
-		var v V
-		return v, false
+		return KV[I, V]{}, false
 	}
-	return n.value, true
+	return newKV(n), true
 }
 
 // Each calls 'fn' on every element in the tree, and its corresponding
 // interval, in order sorted by starting position.
-func (t *Tree[V]) Each(fn func(low, high int, val V)) {
+func (t *Tree[I, V]) Each(fn func(low, high I, val V)) {
 	t.root.each(fn)
 }
 
 // Height returns the height of the tree.
-func (t *Tree[V]) Height() int {
+func (t *Tree[I, V]) Height() int {
 	return t.root.getHeight()
 }
 
 // Size returns the number of elements in the tree.
-func (t *Tree[V]) Size() int {
+func (t *Tree[I, V]) Size() int {
 	return t.root.size()
 }
 
-type node[V any] struct {
-	key   intrvl
+type node[I constraints.Ordered, V any] struct {
+	key   intrvl[I]
 	value V
 
-	max    int
 	height int
-	left   *node[V]
-	right  *node[V]
+	left   *node[I, V]
+	right  *node[I, V]
+
+	// max is highest upper bound of all intervals stored in subtree which
+	// node as its root.
+	max I
 }
 
-func (n *node[V]) add(key intrvl, value V) *node[V] {
+// insert inserts interval key associated with value value to the tree.
+//
+// If interval starting at key.low already exists in a tree, behaviour of this
+// method is defined by overwrite parameter. If it's true, the value is
+// replaced. Otherwise whole subtree is left unchanged.
+//
+// This method returns new root node of a subtree rooted in n after insertion,
+// an interval starting at key.low which already exists in the subtree and a
+// flag if such an interval exists.
+func (n *node[I, V]) insert(
+	key intrvl[I],
+	value V,
+	overwrite bool,
+) (*node[I, V], KV[I, V], bool) {
 	if n == nil {
-		return &node[V]{
+		return &node[I, V]{
 			key:    key,
 			value:  value,
-			max:    key.High,
+			max:    key.high,
 			height: 1,
 			left:   nil,
 			right:  nil,
-		}
+		}, KV[I, V]{}, false
 	}
 
-	if key.Low < n.key.Low {
-		n.left = n.left.add(key, value)
-	} else if key.Low > n.key.Low {
-		n.right = n.right.add(key, value)
+	var kv KV[I, V]
+	var evicted bool
+	if key.low < n.key.low {
+		n.left, kv, evicted = n.left.insert(key, value, overwrite)
+	} else if key.low > n.key.low {
+		n.right, kv, evicted = n.right.insert(key, value, overwrite)
 	} else {
+		if !overwrite {
+			return n, newKV(n), true
+		}
+
+		kv, evicted = newKV(n), true
+
+		n.key = key
 		n.value = value
 	}
-	return n.rebalanceTree()
+
+	return n.rebalanceTree(), kv, evicted
 }
 
-func (n *node[V]) updateMax() {
-	if n != nil {
-		if n.right != nil {
-			n.max = g.Max(n.max, n.right.max)
-		}
-		if n.left != nil {
-			n.max = g.Max(n.max, n.left.max)
-		}
-	}
-}
-
-func (n *node[V]) remove(pos int) *node[V] {
+func (n *node[I, V]) updateMax() {
 	if n == nil {
-		return nil
+		return
 	}
-	if pos < n.key.Low {
-		n.left = n.left.remove(pos)
-	} else if pos > n.key.Low {
-		n.right = n.right.remove(pos)
+
+	n.max = n.key.high
+	if n.right != nil {
+		n.max = generic.Max(n.max, n.right.max)
+	}
+	if n.left != nil {
+		n.max = generic.Max(n.max, n.left.max)
+	}
+}
+
+// remove removes interval starting at pos from a subtree. This function returns
+// the new root of subtree rooted in n after pos removal, the KV removed and an
+// information if any deletion happened (i.e. if interval starting at pos
+// exists).
+func (n *node[I, V]) remove(low I) (*node[I, V], KV[I, V], bool) {
+	if n == nil {
+		return nil, KV[I, V]{}, false
+	}
+
+	var kv KV[I, V]
+	var removed bool
+	if low < n.key.low {
+		n.left, kv, removed = n.left.remove(low)
+	} else if low > n.key.low {
+		n.right, kv, removed = n.right.remove(low)
 	} else {
-		if n.left != nil && n.right != nil {
-			rightMinNode := n.right.findSmallest()
-			n.key = rightMinNode.key
-			n.value = rightMinNode.value
-			n.right = n.right.remove(rightMinNode.key.Low)
-		} else if n.left != nil {
-			n = n.left
-		} else if n.right != nil {
-			n = n.right
-		} else {
-			n = nil
-			return n
-		}
-
+		kv, removed = newKV(n), true
+		n = n.removeThis()
 	}
-	return n.rebalanceTree()
+
+	return n.rebalanceTree(), kv, removed
 }
 
-func (n *node[V]) search(pos int) *node[V] {
+// removeThis deletes n from subtree rooted in n and returns new root of the
+// subtree.
+func (n *node[I, V]) removeThis() *node[I, V] {
+	// This can return nil if n has no children (n.right == nil).
+	if n.left == nil {
+		return n.right
+	}
+	if n.right == nil {
+		return n.left
+	}
+
+	rightMinNode := n.right.findSmallest()
+	n.key = rightMinNode.key
+	n.value = rightMinNode.value
+	n.right, _, _ = n.right.remove(rightMinNode.key.low)
+
+	return n
+}
+
+func (n *node[I, V]) search(low I) *node[I, V] {
 	if n == nil {
 		return nil
 	}
-	if pos < n.key.Low {
-		return n.left.search(pos)
-	} else if pos > n.key.Low {
-		return n.right.search(pos)
+
+	if low < n.key.low {
+		return n.left.search(low)
+	} else if low > n.key.low {
+		return n.right.search(low)
 	} else {
 		return n
 	}
 }
 
-func (n *node[V]) overlaps(key intrvl, result []V) []V {
+func (n *node[I, V]) overlaps(key intrvl[I], result []KV[I, V]) []KV[I, V] {
 	if n == nil {
 		return result
 	}
 
-	if key.Low >= n.max {
+	if key.low >= n.max {
 		return result
 	}
 
 	result = n.left.overlaps(key, result)
 
-	if overlaps(n.key, key.Low, key.High) {
-		result = append(result, n.value)
+	if overlaps(n.key, key) {
+		result = append(result, newKV(n))
 	}
 
-	if key.High <= n.key.Low {
+	if key.high <= n.key.low {
 		return result
 	}
 
@@ -194,27 +275,27 @@ func (n *node[V]) overlaps(key intrvl, result []V) []V {
 	return result
 }
 
-func (n *node[V]) each(fn func(low, high int, val V)) {
+func (n *node[I, V]) each(fn func(low, high I, val V)) {
 	if n == nil {
 		return
 	}
 	n.left.each(fn)
-	fn(n.key.Low, n.key.High, n.value)
+	fn(n.key.low, n.key.high, n.value)
 	n.right.each(fn)
 }
 
-func (n *node[V]) getHeight() int {
+func (n *node[I, V]) getHeight() int {
 	if n == nil {
 		return 0
 	}
 	return n.height
 }
 
-func (n *node[V]) recalculateHeight() {
-	n.height = 1 + g.Max(n.left.getHeight(), n.right.getHeight())
+func (n *node[I, V]) recalculateHeight() {
+	n.height = 1 + generic.Max(n.left.getHeight(), n.right.getHeight())
 }
 
-func (n *node[V]) rebalanceTree() *node[V] {
+func (n *node[I, V]) rebalanceTree() *node[I, V] {
 	if n == nil {
 		return n
 	}
@@ -236,7 +317,7 @@ func (n *node[V]) rebalanceTree() *node[V] {
 	return n
 }
 
-func (n *node[V]) rotateLeft() *node[V] {
+func (n *node[I, V]) rotateLeft() *node[I, V] {
 	newRoot := n.right
 	n.right = newRoot.left
 	newRoot.left = n
@@ -248,7 +329,7 @@ func (n *node[V]) rotateLeft() *node[V] {
 	return newRoot
 }
 
-func (n *node[V]) rotateRight() *node[V] {
+func (n *node[I, V]) rotateRight() *node[I, V] {
 	newRoot := n.left
 	n.left = newRoot.right
 	newRoot.right = n
@@ -260,7 +341,7 @@ func (n *node[V]) rotateRight() *node[V] {
 	return newRoot
 }
 
-func (n *node[V]) findSmallest() *node[V] {
+func (n *node[I, V]) findSmallest() *node[I, V] {
 	if n.left != nil {
 		return n.left.findSmallest()
 	} else {
@@ -268,7 +349,7 @@ func (n *node[V]) findSmallest() *node[V] {
 	}
 }
 
-func (n *node[V]) size() int {
+func (n *node[I, V]) size() int {
 	s := 1
 	if n.left != nil {
 		s += n.left.size()
--- interval/itree_test.go
+++ interval/itree_test.go
@@ -17,15 +17,64 @@ func TestOverlaps(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run("test", func(t *testing.T) {
-			if overlaps(intrvl{tt.l1, tt.h1}, tt.l2, tt.h2) != tt.expect {
+			if overlaps(newIntrvl(tt.l1, tt.h1), newIntrvl(tt.l2, tt.h2)) != tt.expect {
 				t.Fatalf("[%d, %d) vs [%d, %d): expected %v, got %v", tt.l1, tt.h1, tt.l2, tt.h2, tt.expect, !tt.expect)
 			}
 		})
 	}
 }
 
+func TestPut(t *testing.T) {
+	tree := New[int, string]()
+	tree.Put(5, 7, "foo1")
+	tree.Put(5, 9, "foo2")
+	tree.Put(2, 4, "foo3")
+	tree.Put(8, 9, "foo4")
+
+	tests := []struct {
+		low, high int
+		vals      []string
+	}{{
+		low:  6,
+		high: 7,
+		vals: []string{"foo2"},
+	}, {
+		low:  7,
+		high: 8,
+		vals: []string{"foo2"},
+	}, {
+		low:  8,
+		high: 9,
+		vals: []string{"foo2", "foo4"},
+	}, {
+		low:  3,
+		high: 6,
+		vals: []string{"foo3", "foo2"},
+	}}
+
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("test_%d", i), func(t *testing.T) {
+			ov := tree.Overlaps(tt.low, tt.high)
+			if len(ov) != len(tt.vals) {
+				t.Fatalf("Len missmatch: expected %d, got %d",
+					len(tt.vals), len(ov))
+			}
+
+			for i, v := range tt.vals {
+				if ov[i].Val == v {
+					continue
+				}
+
+				t.Fatalf("Value mismatch at position %d: expected %q, got %q",
+					i, v, ov[i].Val)
+			}
+		})
+	}
+}
+
 func Example() {
-	tree := New[string]()
+	tree := New[int, string]()
 	tree.Put(0, 10, "foo")
 	tree.Put(5, 9, "bar")
 	tree.Put(10, 11, "baz")
@@ -33,7 +82,7 @@ func Example() {
 
 	vals := tree.Overlaps(4, 10)
 	for _, v := range vals {
-		fmt.Println(v)
+		fmt.Println(v.Val)
 	}
 	// Output:
 	// foo

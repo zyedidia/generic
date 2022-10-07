3e4a8416c91fc62bef06bf6d47413ffb95d2977b
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2022-10-07 18:56:41 +0200

    Added methods to Queue

--- queue/queue.go
+++ queue/queue.go
@@ -9,7 +9,8 @@ import (
 
 // Queue is a simple First In First Out (FIFO) queue.
 type Queue[T any] struct {
-	list *list.List[T]
+	list   *list.List[T]
+	length int
 }
 
 // New returns an empty First In First Out (FIFO) queue.
@@ -19,31 +20,92 @@ func New[T any]() *Queue[T] {
 	}
 }
 
+// Len returns the number of items currently in the queue.
+func (q *Queue[T]) Len() int {
+	return q.length
+}
+
 // Enqueue inserts 'value' to the end of the queue.
 func (q *Queue[T]) Enqueue(value T) {
 	q.list.PushBack(value)
+	q.length++
 }
 
 // Dequeue removes and returns the item at the front of the queue.
 //
 // A panic occurs if the queue is Empty.
 func (q *Queue[T]) Dequeue() T {
+	value, ok := q.TryDequeue()
+	if !ok {
+		panic("queue: tried to dequeue from an empty queue")
+	}
+	return value
+}
+
+// TryDequeue tries to remove and return the item at the front of the queue.
+//
+// If the queue is empty, then false is returned as the second return value.
+func (q *Queue[T]) TryDequeue() (T, bool) {
+	if q.Empty() {
+		var zero T
+		return zero, false
+	}
 	value := q.list.Front.Value
 	q.list.Remove(q.list.Front)
+	q.length--
+	return value, true
+}
 
-	return value
+// DequeueAll removes and returns all the items in the queue.
+func (q *Queue[T]) DequeueAll() []T {
+	slice := make([]T, q.length)
+	for i := 0; i < q.length; i++ {
+		slice[i] = q.Dequeue()
+	}
+	return slice
 }
 
 // Peek returns the item at the front of the queue without removing it.
 //
 // A panic occurs if the queue is Empty.
 func (q *Queue[T]) Peek() T {
+	if q.Empty() {
+		panic("queue: tried to peek an empty queue")
+	}
 	return q.list.Front.Value
+}
+
+// TryPeek tries to return the item at the front of the queue without removing it.
+//
+// If the queue is empty, then false is returned as the second return value.
+func (q *Queue[T]) TryPeek() (T, bool) {
+	if q.Empty() {
+		var zero T
+		return zero, false
+	}
+	return q.list.Front.Value, true
+}
+
+// PeekAll returns all the items in the queue without removing them.
+func (q *Queue[T]) PeekAll() []T {
+	slice := make([]T, q.length)
+	var index int
+	q.list.Front.Each(func(val T) {
+		slice[index] = val
+		index++
+	})
+	return slice
 }
 
 // Empty returns true if the queue is empty.
 func (q *Queue[T]) Empty() bool {
 	return q.list.Front == nil
+}
+
+// Clear empties the queue, resetting it to zero elements.
+func (q *Queue[T]) Clear() {
+	q.length = 0
+	q.list = list.New[T]()
 }
 
 // Each calls 'fn' on every item in the queue, starting with the least
